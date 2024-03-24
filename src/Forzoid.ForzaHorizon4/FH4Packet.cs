095601b9f7f10fd01da7dc8ce1a7d707a686c743
Kingloo <k1ngloo@hotmail.com>
2024-03-24 21:34:16 +0100

    FH4: add project reference to .Common and FH4Packet.cs

---
+++ src/Forzoid.ForzaHorizon4/FH4Packet.cs
@@ -0,0 +1,29 @@
+using System;
+using Forzoid.Common;
+
+namespace Forzoid.ForzaHorizon4
+{
+	public class FH4Packet
+	{
+		public Packet RawPacket { get; }
+		public IGame Game { get; }
+		// public FH4Dash Dash { get; init; }
+		// public FH4Sled Sled { get; init; }
+
+		public FH4Packet(Packet packet)
+		{
+			ArgumentNullException.ThrowIfNull(packet);
+
+			RawPacket = packet;
+
+			Game = new Game(
+				fullName: "Forza Horizon 4",
+				shortName: "FH4",
+				releaseYear: 2018
+			);
+
+			// Dash = FH4Dash.Create(packet.Data.Span);
+			// Sled = FH4Sled.Create(packet.Data.Span);
+		}
+	}
+}
